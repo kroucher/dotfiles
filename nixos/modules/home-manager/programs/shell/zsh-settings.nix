1c189758c07aa079fb3cae7936a7774e90e56d97
kroucher <kroucher.1019@gmail.com>
2024-03-07 18:27:11 +0100

    add zsh suggestions

--- nixos/modules/home-manager/programs/shell/zsh-settings.nix
+++ nixos/modules/home-manager/programs/shell/zsh-settings.nix
@@ -4,7 +4,16 @@
   enableAutosuggestions = true;
   syntaxHighlighting.enable = true;
 
-
+  plugins = [
+    {
+      name = "zsh-autosuggestions";
+      file = "zsh-autosuggestions.plugin.zsh";
+      src = builtins.fetchGit {
+        url = "https://github.com/zsh-users/zsh-autosuggestions";
+        rev = "a411ef3e0992d4839f0732ebeb9823024afaaaa8";
+      };
+    }
+  ];
   shellAliases = {
     ll = "ls -l";
     update = "sudo nixos-rebuild switch";
@@ -14,7 +23,7 @@
   dotDir = ".config/";
   oh-my-zsh = {
     enable = true;
-    plugins = [ "git" ];
+    plugins = [ "git" "rust" "zsh-autosuggestions" ];
     theme = "robbyrussell";
   };
 }
